57bca954749c9b1f76565776ce7fb84b8b601d86
bob <amit.bhandari.c@gmail.com>
2024-09-07 19:29:39 +0530

    Made composable customisable from calling side:

--- app/src/main/java/com/bhandari/progressbar/WaveProgress.kt
+++ app/src/main/java/com/bhandari/progressbar/WaveProgress.kt
@@ -1,5 +1,6 @@
 package com.bhandari.progressbar
 
+import android.util.Range
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
@@ -14,19 +15,26 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.Fill
-import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 import kotlinx.coroutines.launch
 import kotlin.math.PI
 import kotlin.math.sin
 
 @Composable
-fun WaveProgress(progress: Float, modifier: Modifier = Modifier) {
+fun WaveProgress(
+    progress: Float,
+    modifier: Modifier = Modifier,
+    amplitudeRange: Range<Float> = Range(20f, 80f),
+    fillBrush: Brush = Brush.horizontalGradient(listOf(Color.Red, Color.Blue)),
+    waveSteps: Int = 20,
+    waveFrequency: Int = 2,
+) {
     val phaseShift = remember { Animatable(0f) }
-    val amplitude = remember { Animatable(20f) }
+    val amplitude = remember { Animatable(amplitudeRange.lower) }
 
     // Coroutine scope for running concurrent animations
     val coroutineScope = rememberCoroutineScope()
@@ -44,7 +52,7 @@ fun WaveProgress(progress: Float, modifier: Modifier = Modifier) {
 
         coroutineScope.launch {
             amplitude.animateTo(
-                targetValue = 80f,
+                targetValue = amplitudeRange.upper,
                 animationSpec = infiniteRepeatable(
                     animation = tween(durationMillis = 2000, easing = LinearEasing),
                     repeatMode = RepeatMode.Reverse
@@ -56,24 +64,15 @@ fun WaveProgress(progress: Float, modifier: Modifier = Modifier) {
     Box(
         modifier = modifier
             .drawBehind {
-                println(amplitude.value)
-                val position = (1 - progress) * size.height
-                //val position = size.height / 2
-                val frequency = 2
-                val step = 2
-
-                val sinePath = Path()
-                prepareSinePath(sinePath, size.width, frequency, amplitude.value, phaseShift.value, position, step)
-                drawPath(path = sinePath, color = Color.Blue, style = Stroke(width = 8f))
+                val yPos = (1 - progress) * size.height
 
                 Path()
                     .apply {
-                        addPath(sinePath)
+                        prepareSinePath(this, size.width, waveFrequency, amplitude.value, phaseShift.value, yPos, waveSteps)
                         lineTo(size.width, size.height)
                         lineTo(0f, size.height)
                         close()
-                        drawPath(path = this, color = Color.Blue, style = Fill)
-                    }
+                    }.also { drawPath(path = it, brush = fillBrush, style = Fill) }
             }
     )
 }
