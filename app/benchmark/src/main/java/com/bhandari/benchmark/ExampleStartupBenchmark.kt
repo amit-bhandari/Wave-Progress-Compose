740ccfc6f4f58917d09c3037fe00eda997022109
bob <amit.bhandari.c@gmail.com>
2024-11-16 18:47:09 +0530

    Code cleanup

--- app/benchmark/src/main/java/com/bhandari/benchmark/ExampleStartupBenchmark.kt
+++ app/benchmark/src/main/java/com/bhandari/benchmark/ExampleStartupBenchmark.kt
@@ -1,10 +1,8 @@
 package com.bhandari.benchmark
 
-import androidx.benchmark.macro.CompilationMode
 import androidx.benchmark.macro.FrameTimingMetric
 import androidx.benchmark.macro.StartupMode
 import androidx.benchmark.macro.StartupTimingMetric
-import androidx.benchmark.macro.TraceSectionMetric
 import androidx.benchmark.macro.junit4.MacrobenchmarkRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
